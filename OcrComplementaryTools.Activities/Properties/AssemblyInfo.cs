aade49bb91c077674285fc8e720ede3470f2d2ad
Swetake <41154848+Swetake@users.noreply.github.com>
2019-07-20 21:14:50 +0900

    Modify assembly info

--- OcrComplementaryTools.Activities/Properties/AssemblyInfo.cs
+++ OcrComplementaryTools.Activities/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("OcrReviewActivities")]
+[assembly: AssemblyTitle("OcrComplementaryToolsActivities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OcrReviewActivities")]
-[assembly: AssemblyCopyright("Copyright ©  Yoichi Suetake (swetake.com) 2018")]
+[assembly: AssemblyProduct("OcrComplementaryToolsActivities")]
+[assembly: AssemblyCopyright("Copyright ©  Yoichi Suetake 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
